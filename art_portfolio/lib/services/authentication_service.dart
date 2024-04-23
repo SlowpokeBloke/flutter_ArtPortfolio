d55fb74f1b0835d96acf2bff5ef31b6a06b0dd38
Abenezer <abitysank@gmail.com>
2024-04-22 23:35:46 -0400

    Update authentication_service.dart

--- art_portfolio/lib/services/authentication_service.dart
+++ art_portfolio/lib/services/authentication_service.dart
@@ -1,5 +1,7 @@
 // Inside of services/authentication_service.dart
 
+// Inside of services/authentication_service.dart
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
