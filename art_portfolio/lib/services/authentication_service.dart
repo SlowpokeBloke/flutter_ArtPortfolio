136ad921422479033b05a7760917746878a00efc
Abenezer <abitysank@gmail.com>
2024-04-23 18:52:31 -0400

    Update authentication_service.dart
    auth update

--- art_portfolio/lib/services/authentication_service.dart
+++ art_portfolio/lib/services/authentication_service.dart
@@ -1,6 +1,6 @@
+// Inside of services/authentication_service.dart
+
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthenticationService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
