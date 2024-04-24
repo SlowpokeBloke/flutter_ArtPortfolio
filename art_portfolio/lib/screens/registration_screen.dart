81b1654c97eabb8e353712a12799fc57e0362474
Abenezer <abitysank@gmail.com>
2024-04-23 22:00:45 -0400

    Update registration_screen.dart

--- art_portfolio/lib/screens/registration_screen.dart
+++ art_portfolio/lib/screens/registration_screen.dart
@@ -1,5 +1,4 @@
 // inside of screens/registration_screen.dart
-
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
