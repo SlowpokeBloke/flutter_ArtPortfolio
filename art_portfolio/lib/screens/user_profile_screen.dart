bbd8c8559634ee399b7f9f98efc134eadfad3f16
Abenezer <abitysank@gmail.com>
2024-04-25 16:28:51 -0400

    Update user_profile_screen.dart

--- art_portfolio/lib/screens/user_profile_screen.dart
+++ art_portfolio/lib/screens/user_profile_screen.dart
@@ -5,11 +5,16 @@ import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class UserProfileScreen extends StatelessWidget {
+class UserProfileScreen extends StatefulWidget {
+  @override
+  _UserProfileScreenState createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
   final DBHelper _dbHelper = DBHelper();
   final ImagePicker _picker = ImagePicker();
-
-  UserProfileScreen({Key? key}) : super(key: key);
+  String?
+      _firstCollectionImageUrl; // Local state to store the first image URL of the new collection
 
   Future<void> _addArtwork(BuildContext context) async {
     final FirebaseAuth auth = FirebaseAuth.instance;
@@ -19,11 +24,13 @@ class UserProfileScreen extends StatelessWidget {
     if (artistId.isNotEmpty) {
       final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
       if (image != null) {
-        String imageUrl = await _dbHelper.uploadImage(File(image.path), "artwork_path/$artistId");
+        String imageUrl = await _dbHelper.uploadImage(
+            File(image.path), "artwork_path/$artistId");
         await _dbHelper.addArtwork('Artwork Title', imageUrl, artistId);
       }
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("No user logged in.")));
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("No user logged in.")));
     }
   }
 
@@ -37,18 +44,26 @@ class UserProfileScreen extends StatelessWidget {
       if (images != null && images.length >= 2) {
         List<String> imageUrls = [];
         for (var image in images) {
-          String imageUrl = await _dbHelper.uploadImage(File(image.path), "collection_path/$artistId");
+          String imageUrl = await _dbHelper.uploadImage(
+              File(image.path), "collection_path/$artistId");
           imageUrls.add(imageUrl);
         }
         await _dbHelper.addCollection('New Collection', imageUrls, artistId);
+        setState(() {
+          _firstCollectionImageUrl = imageUrls
+              .first; // Update the local state with the first image URL
+        });
         Navigator.of(context).push(MaterialPageRoute(
           builder: (context) => CollectionDetailsScreen(imageUrls),
         ));
       } else {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Please select at least 2 images for the collection.")));
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content:
+                Text("Please select at least 2 images for the collection.")));
       }
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("No user logged in.")));
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("No user logged in.")));
     }
   }
 
@@ -64,20 +79,24 @@ class UserProfileScreen extends StatelessWidget {
         actions: [
           IconButton(
             icon: Icon(Icons.edit),
-            onPressed: () => Navigator.pushNamed(context, '/editProfile'),
+            onPressed: () => Navigator.pushNamed(context, '/edit_profile'),
           ),
         ],
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+
+
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const Padding(
                   padding: EdgeInsets.all(8.0),
-                  child: Text('Works', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                  child: Text('Works',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
                 ),
                 Expanded(
                   child: StreamBuilder(
@@ -91,20 +110,25 @@ class UserProfileScreen extends StatelessWidget {
                       }
 
                       List<Widget> children = [
+
+                        
                         Padding(
                           padding: EdgeInsets.only(left: 16.0),
                           child: GestureDetector(
                             onTap: () => _addArtwork(context),
-                            child: Column( // Wrap the button and text in a Column
+                            child: Column(
+                              // Wrap the button and text in a Column
                               children: [
                                 SizedBox(
+
                                   width: 130,
-                                  height: 260, // Adjusted height to accommodate text below the button
+                                  height:260, // Adjusted height to accommodate text below the button
                                   child: Container(
                                     decoration: BoxDecoration(
                                       borderRadius: BorderRadius.circular(8),
                                       image: const DecorationImage(
-                                        image: AssetImage('assets/Add_ArtWork.png'),
+                                        image: AssetImage(
+                                            'assets/Add_ArtWork.png'),
                                         fit: BoxFit.cover,
                                       ),
                                     ),
@@ -116,8 +140,12 @@ class UserProfileScreen extends StatelessWidget {
                                       ),
                                     ),
                                   ),
+
+
                                 ),
-                                const SizedBox(height: 5), // Spacing between the button and text
+                                const SizedBox(
+                                    height:
+                                        5), // Spacing between the button and text
                                 const Text(
                                   'Add Artwork', // Text label for the button
                                   style: TextStyle(
@@ -160,88 +188,120 @@ class UserProfileScreen extends StatelessWidget {
             ),
           ),
 
+Expanded(
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text(
+          'Collections',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      Expanded(
+        child: StreamBuilder(
+          stream: _dbHelper.getCollections(artistId),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
 
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Collections', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                  Expanded(
-                    child: StreamBuilder(
-                      stream: _dbHelper.getCollections(artistId),
-                      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                        if (snapshot.connectionState == ConnectionState.waiting) {
-                          return Center(child: CircularProgressIndicator());
-                        }
-                        if (snapshot.hasError) {
-                          return Text('Error: ${snapshot.error}');
-                        }
-                        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                          // Placeholder for adding new collections
-                          return Column(
-                            children: [
-                              InkWell(
-                                onTap: () => _addCollection(context),
-                                child: Container(
-                                  height: 200,
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey[300],
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  child: Center(
-                                    child: Text('No collections found. Tap to add a new collection.', style: TextStyle(color: Colors.black)),
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: ElevatedButton(
-                                  onPressed: () => _addCollection(context),
-                                  child: Text('Add Collection'),
-                                ),
-                              ),
-                            ],
-                          );
-                        }
-
-                        return ListView.builder(
-                          itemCount: snapshot.data!.docs.length,
-                          itemBuilder: (context, index) {
-                            var collection = snapshot.data!.docs[index];
-                            List<String> imageUrls = List<String>.from(collection['imageUrls']); // Assuming 'imageUrls' is a list of strings stored in your collection document
-                            return ListTile(
-                              title: Text(collection['title']),
-                              leading: Image.network(collection['imageUrl'], width: 100, height: 100, fit: BoxFit.cover), // Displaying the collection thumbnail
-                              onTap: () {
-                                // Navigate to collection detail screen and display it
-                                Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => CollectionDetailsScreen(imageUrls),
-                                ));
-                              },
-                            );
-                          },
-                        );
-                      },
+            List<Widget> children = [
+              Padding(
+                padding: EdgeInsets.only(left: 16.0),
+                child: GestureDetector(
+                  onTap: () => _addCollection(context),
+                  child: Container(
+                    width: 130,
+                    height: 260,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      image: const DecorationImage(
+                        image: AssetImage('assets/placeholder.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: const Center(
+                      child: Icon(
+                        Icons.add,
+                        size: 50,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-          ),
+            ];
+
+            if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+              snapshot.data!.docs.forEach((documentSnapshot) {
+                Map<String, dynamic> collectionData = documentSnapshot.data() as Map<String, dynamic>;
+                List<String> imageUrls = List<String>.from(collectionData['imageUrls'] ?? []);
+                ImageProvider<Object> imageProvider = imageUrls.isNotEmpty
+                    ? NetworkImage(imageUrls.first) as ImageProvider<Object>
+                    : const AssetImage('assets/placeholder.png') as ImageProvider<Object>;
+
+                children.add(
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CollectionDetailsScreen(imageUrls),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      margin: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        image: DecorationImage(
+                          image: imageProvider,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              });
+            } else {
+              children.add(
+                Center(
+                  child: Text('No collections found'),
+                ),
+              );
+            }
+
+            return ListView(
+              scrollDirection: Axis.horizontal,
+              children: children,
+            );
+          },
+        ),
+      ),
+    ],
+  ),
+),
 
 
 
 
 
+
+
+
+          
         ],
       ),
     );
   }
 }
-
-
 
 // You might need a details screen for collections to show all images
 class CollectionDetailsScreen extends StatelessWidget {
