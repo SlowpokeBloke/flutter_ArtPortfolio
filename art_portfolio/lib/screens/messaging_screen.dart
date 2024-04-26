6763ba136bd743c5b49dff5579dbcd74afea9f81
mcoca <mcoca4280@gmail.com>
2024-04-26 17:24:55 -0400

    added messaging functionality (needs input on nav)

--- art_portfolio/lib/screens/messaging_screen.dart
+++ art_portfolio/lib/screens/messaging_screen.dart
@@ -1,13 +1,107 @@
+import 'package:art_portfolio_showcase/services/messaging_service.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../firebase_options.dart';
 
-class MessagingScreen extends StatelessWidget {
-  const MessagingScreen({Key? key}) : super(key: key);
+
+class MessagingScreen extends StatefulWidget {
+  const MessagingScreen({Key? key, required this.recipientName, required this.recipientId}) : super(key: key);
+  //recipient user info vars here
+  final String recipientName;
+  final String recipientId;
+  @override
+  _MessagingScreenState createState() => _MessagingScreenState();
+}
+
+class _MessagingScreenState extends State<MessagingScreen>{
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  final TextEditingController _msgController = TextEditingController();
+  final MessagingService _msgService = MessagingService();
+
+  void sendMessage() async {
+    if(_msgController.text.isNotEmpty){
+      await _msgService.sendMessage(widget.recipientId, _msgController.text);
+      _msgController.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('MessagingScreen')),
-      body: Center(child: Text('Messaging Screen')),
+      body: Column(
+        children: [
+          Expanded(child: _buildMsgList(),),
+          _buildMsgInput(),
+        ]
+        // ListView.builder(
+        //   itemCount: rooms.length,
+        //   itemBuilder: (context, index) {
+        //     Room item = rooms[index];
+        //     return ListTile(
+        //       title: Text(item.roomName),
+        //       onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context) => MessageScreen(roomId: item.roomId,)));},
+        //     );
+        //   },
+        // ),
+      ),
+    );
+  }
+
+  //build msg list
+  Widget _buildMsgList() {
+    return StreamBuilder(
+      stream: _msgService.getMessages(widget.recipientId, _firebaseAuth.currentUser!.uid), 
+      builder: (context, snapshot) {
+        if(snapshot.hasError){
+          return Text('Error$snapshot.error');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting){
+          return const Text("Loading...");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs.map((doc) => _buildMsgItem(doc)).toList(),
+
+        );
+    });
+  }
+
+  //build msg item
+  Widget _buildMsgItem(DocumentSnapshot doc){
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    //msgs from curr user aligned right, else left align
+    var align = (data['senderId'] == _firebaseAuth.currentUser!.uid) ? Alignment.centerRight : Alignment.centerLeft;
+    
+    return Container(
+      alignment: align,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: (data['senderId'] == _firebaseAuth.currentUser!.uid)? CrossAxisAlignment.end : CrossAxisAlignment.start,
+          //mainAxisAlignment: (data['senderId'] == _firebaseAuth.currentUser!.uid)? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            Text(data['senderEmail']),
+            Text(data['message']),
+          ],
+        ), 
+      )
+    );
+
+  }
+
+  //buld msg input
+  Widget _buildMsgInput() {
+    return Row(
+      children: [
+        //txt field
+        Expanded(child: TextField(controller: _msgController, obscureText: false,)),
+        //send btn
+        IconButton(onPressed: sendMessage, icon: const Icon(Icons.send))
+      ],
     );
   }
 }
---
+++ art_portfolio/lib/widgets/messages.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../firebase_options.dart';
+
+class Message {
+  final String senderId;
+  final String senderEmail;
+  final String receiverId;
+  final String msgBody;
+  final Timestamp timestamp;
+  Message({
+    required this.senderId,
+    required this.senderEmail,
+    required this.receiverId,
+    required this.msgBody,
+    required this.timestamp,
+  });
+  Map<String, dynamic> toMap() {
+    return {
+      'senderId': senderId,
+      'senderEmail': senderEmail,
+      'receiverId': receiverId,
+      'msgBody' : msgBody,
+      'timestamp':timestamp,
+    };
+  }
+}
+// class MessageScreen extends StatefulWidget {
+//   const MessageScreen({Key? key, required this.roomId}) : super(key: key);
+//   final int roomId;
+//   final String title = "Chat";
+
+//   @override
+//   _MessageScreenState createState() => _MessageScreenState();
+// }
+
+// class _MessageScreenState extends State<MessageScreen> {
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+//   //method to sign out from firebase
+//   void _signOut() async {
+//     await _auth.signOut();
+//     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+//       content: const Text('Signed out successfully'),
+//     ));
+//     Navigator.pop(context);
+//   }
+
+//   late TextEditingController controller;
+
+//   @override
+//   void initState(){
+//     super.initState();
+//     controller = TextEditingController();
+//   }
+
+//   @override
+//   void dispose() {
+//     controller.dispose();
+//     super.dispose();
+//   }
+
+//   Future<String?> openDialog() => showDialog<String>(
+//     context: context,
+//     builder: (context) => AlertDialog(
+//       title: const Text('Create A New Message'),
+//       content: TextField(
+//         autofocus: true,
+//         decoration: const InputDecoration(hintText: 'Enter Message Name'),
+//         controller: controller,
+//       ),
+//       actions: [
+//         TextButton(onPressed: submit, child: const Text('SUBMIT'))
+//       ],
+//     ),
+//   );
+//   void submit(){
+//     FirebaseFirestore.instance.collection('msgs').doc('test_room').collection('messages').add({
+//       'msgBody' : controller.text,
+//       'senderId' : "none",
+//     });
+//     Navigator.of(context).pop(controller.text);
+//   }
+//   void _createRoom() async {
+//     String? newMessage = await openDialog();
+//     setState(() {
+//       controller.clear();
+//       msgs.add(Message(newMessage.toString(), msgs.length));
+//     });
+//   }
+//   List<Message> msgs = [Message("msg1", 1), Message("msg2", 2)];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//         actions: <Widget>[
+//           ElevatedButton(
+//             onPressed: () {
+//               _signOut();
+//             },
+//             child: const Text('Sign Out'),
+//           ),
+//         ],
+//       ),
+//       body: Center(
+//         child: ListView.builder(
+//           itemCount: msgs.length,
+//           itemBuilder: (context, index) {
+//             Message item = msgs[index];
+//             return ListTile(
+//               leading: Text("Sender: ${_auth.currentUser?.email}"),
+//               title: Text(item.msgBody),
+//             );
+//           },
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _createRoom,
+//         tooltip: 'Send A Message',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
