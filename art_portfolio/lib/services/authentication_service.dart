f8f50766e56388b62cee3a6ba9d49384662da7ad
mcoca <mcoca4280@gmail.com>
2024-04-27 16:52:12 -0400

    restored firestore import in auth service

--- art_portfolio/lib/services/authentication_service.dart
+++ art_portfolio/lib/services/authentication_service.dart
@@ -1,6 +1,7 @@
 // Inside of services/authentication_service.dart
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthenticationService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
